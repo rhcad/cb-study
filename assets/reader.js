916bd3807488b06f38526a8e8cd7e80ba4e8a81b
Zhang Yungui <rhcad@hotmail.com>
2022-07-27 21:17:23 +0800

    Update

--- assets/reader.js
+++ assets/reader.js
@@ -853,7 +853,7 @@ function getNoteContent(note, title, rows, rawNote, desc, tag) {
         text = note[i + 2].replace(/\d{4}\w*$/, '').split(/\n/g),
         moreText = note1.substring(3, note1.length - 1),
         orgText = !rawNote && note1.length > 5 && !autoMore? note1.substring(0, 3) +
-            `<span class="more" data-more="${moreText}">${moreText.indexOf('(') > 0 ? moreText : '…'}</span>` + note1[note1.length - 1] : title_;
+            `<span class="more" data-more="${moreText}">${/[()]/.test(moreText) ? moreText : '…'}</span>` + note1[note1.length - 1] : title_;
     let noteText = `<span class="note-text">${text[0]}</span>`,
       nextText = text.slice(1).map(t => `<p class="note-text note-text2">${t}</p>`).join('');
     const head = `<div data-id="${note[i]}" data-line-no="${line}" data-tag="${tag}" class="note-item${rawNote ? ' note-raw' : ''}">`;
